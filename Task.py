d4ca6f0188ce3ce886961e7134596d8776b5171a
Robinson Nathaniel <robinsonn@avast.com>
2020-04-28 15:04:15 +0100

    New tasks

--- Task.py
+++ Task.py
@@ -1,7 +1,7 @@
 import requests
 import re
 #regex = r"^[^{]*{([^}]*)}.*$"
-
+ips_read = []
 array = []
 try:
     ret = requests.get('https://raw.githubusercontent.com/chromium/chromium/8574431a884cc99dfb7bdaaeb6cbf3b297fd9fba/net/dns/public/doh_provider_list.cc')
@@ -16,14 +16,13 @@ start = source.find('providers{{')
 source = source[start+11:]
 end = source.find('}};')
 source = source[:end]
-
 print (source)
 
+f = open("taskfile.rtf", "a")
 for part in source.split('DohProviderEntry('):
     end1 = part.find('},')
     select = part[:end1+2]
     
-    #matches = re.sub(regex, select, re.MULTILINE)
     match = re.findall(r"^[^{]*{([^}]*)}.*$", select.strip())
     ips = []
     try: 
@@ -31,27 +30,40 @@ for part in source.split('DohProviderEntry('):
     except IndexError as e:
        pass
     array = array+ips
-   
+    f.write(str(ips))
+
+f.close()
+
+f = open('taskfile.rtf', 'r')
+count = 0
+
+while True:
+    count +=1
+    ips_read = f.readlines()
+
+    if not ips_read:
+        break
+    print(ips_read)
+
+f.close()
 
 
-print(array)
-    
 
+f = open("taskfile.rtf","r+")
+f.truncate(0)
+f.close()
 
 #print("----------")
 
 #print(array)
-f = open("taskfile.rtf", "a")
-f.write(array)
-f.close()
-f = open('taskfile.rtf', 'r')
+#f = open("taskfile.rtf", "a")
+#f.write(array)
+#f.close()
+#f = open('taskfile.rtf', 'r')
 #lines = f.readlines()
 
 #f.close()
 
 #print (select)   
 
-#f = open("taskfile.rtf","r+")
-#f.truncate(0)
-#f.close()   
-
+#matches = re.sub(regex, select, re.MULTILINE)
